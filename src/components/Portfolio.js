7817a0b950de23a749bc8038fac3d17b95da9245
Autumn Gehring <autumngehring@Autumns-MacBook-Pro.local>
2021-02-13 21:08:59 -0700

    updates

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -17,13 +17,13 @@ export default class Porfolio extends Component {
                                                             <h5>Project Management</h5>
                                                             <h5>Power and PDU Control</h5>
                                                             <h5>Networking</h5>
-                                                            <h5>Fiber Pulling, Terminating and Testing</h5>
+                                                            <h5>Fiber Pulling and Testing</h5>
                                                         
                                                             <h5>Cable Pulling, Terminating and Testing</h5>
                                                             <h5>Operations Management</h5>
                                                             <h5>Critical Facitilies</h5>
                                                             <h5>On-Call for Emergency</h5>
-                                                            <h5>Maintain and Clean and Function Data Center Facility</h5>
+                                                            <h5>Maintain and Clean Data Center Facility</h5>
                                                             <h5>Ensure All Data Is Secure</h5>
                                                               
                                                     </div>
