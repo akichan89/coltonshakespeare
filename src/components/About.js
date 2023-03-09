bf13842c28ed9f005647bcfdd9b3804d57533f5a
akichan89 <62314744+akichan89@users.noreply.github.com>
2023-03-08 17:25:01 -0700

    Update About.js
    fixing spelling errors

--- src/components/About.js
+++ src/components/About.js
@@ -18,14 +18,36 @@ export default class About extends Component {
                     <div className="nine columns main-col">
                         <div className="aboutHeader">About Me:</div>
                         <div className="aboutMe">
-                        <p>Let me tell you a little bit about myself: I am a hard worker, determined and love to live a positive life. 
-                        I currently work as a Data Center Operations Manager at DataBank, located in Utah. 
-                        I am a self trained IT Professional, currently studying towwards various industry standard certifications. With over 10 years of Data Center and Management experience, I am capable of problem solving, delivering quality results, and grooming exemplary technicians. What does this mean for you? Well, these skills would be used
-                        assisting in creating long lasting customer relationships and efficient Data Centers; which is how expections would be exceeded
-                        at your company.</p>
- 
-                        <p>I am also a very fun, upbeat, creative individual that create a positive environment and put a smile on anyone’s 
-                            face. I feel I would be a great asset to your company.</p>
+                        <p>Let me tell you a little bit about myself: I am a hard worker, determined and love to live a positive life.
+
+
+
+
+                       I currently work as a Data Center Operations Manager at DataBank, located in Utah.
+
+
+                       I am a self trained IT Professional, currently studying towards various industry standard certifications. With over 10 years of Data Center and Management experience, I am capable of problem solving, delivering quality results, and grooming exemplary technicians. What does this mean for you? Well, these skills would be used
+
+
+                       assisting in creating long lasting customer relationships and efficient Data Centers; which is how expectations would be exceeded
+
+
+                       at your company.</p>
+
+
+
+
+
+
+                       <p>I am also a very fun, upbeat, creative individual that create a positive environment and put a smile on anyone’s
+
+
+                           face. I feel I would be a great asset to your company.</p>
+
+
+                       <p>I would love to hear from you! Let's t
+
+
                         <p>I would love to hear from you! Let's talk!</p>
 
                         </div>
